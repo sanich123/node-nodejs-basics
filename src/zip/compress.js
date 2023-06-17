ecb5f2c402339382a1b9f39cc8a87fee77dd12f0
sanich123 <aorovonin.piano@gmail.com>
2023-06-17 10:47:22 +0300

    feat: solve a task with zipping the file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,28 @@
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { createGzip } from "zlib";
+import { isFileExist, isFileNotExist } from "../utils/utils.js";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const sourceFile = join(__dirname, "files/fileToCompress.txt");
+  const destinationFile = join(__dirname, "files/archive.gz");
+  isFileExist(sourceFile);
+  isFileNotExist(destinationFile);
+
+  const sourceStream = createReadStream(sourceFile);
+  const destinationStream = createWriteStream(destinationFile);
+
+  pipeline(sourceStream, gzip, destinationStream, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+    }
+  });
 };
 
 await compress();
