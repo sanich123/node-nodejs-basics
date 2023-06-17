4077037213e105f926df11c5c77027d64080f247
sanich123 <aorovonin.piano@gmail.com>
2023-06-17 11:03:31 +0300

    feat: solve unzip case

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import { pipeline } from "stream";
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { isFileExist, isFileNotExist } from "../utils/utils.js";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+  const gzip = createGunzip();
+  const sourceFile = join(__dirname, "files/archive.gz");
+  const destinationFile = join(__dirname, "files/fileToCompress.txt");
+  isFileExist(sourceFile);
+  isFileNotExist(destinationFile);
+
+  const sourceStream = createReadStream(sourceFile);
+    const destinationStream = createWriteStream(destinationFile);
+
+  pipeline(sourceStream, gzip, destinationStream, (err) => {
+    if (err) {
+      console.error("An error occurred:", err);
+    }
+  });
 };
 
 await decompress();
